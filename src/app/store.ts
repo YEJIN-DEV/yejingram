9092b58f4513ea8eeccc8f89df313874dd25e939
Yuhwan Kim <asm6788@naver.com>
2025-08-19 01:39:48 +0900

    IndexDB 저장

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,54 @@
-import { configureStore } from '@reduxjs/toolkit'
-import characterReducer from '../entities/character/slice'
-import roomReducer from '../entities/room/slice'
-import messageReducer from '../entities/message/slice'
-import settingsReducer from '../entities/setting/slice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import localforage from 'localforage';
+import {
+    persistReducer,
+    persistStore,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+
+import characterReducer from '../entities/character/slice';
+import roomReducer from '../entities/room/slice';
+import messageReducer from '../entities/message/slice';
+import settingsReducer from '../entities/setting/slice';
+
+localforage.config({
+    name: 'yejingram',
+    storeName: 'persist',
+});
+
+const persistConfig = {
+    key: 'yejingram',
+    storage: localforage as any, // localForage는 getItem/setItem/removeItem을 제공하므로 호환됩니다.
+    version: 1,
+    whitelist: ['characters', 'rooms', 'messages', 'settings'],
+};
+
+const rootReducer = combineReducers({
+    characters: characterReducer,
+    rooms: roomReducer,
+    settings: settingsReducer,
+    messages: messageReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        characters: characterReducer,
-        rooms: roomReducer,
-        settings: settingsReducer,
-        messages: messageReducer,
-    },
-})
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        // redux-persist가 직렬화 불가능한 액션을 쓰므로 검사 무시 목록을 지정
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
 
-// 타입 추론용
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
